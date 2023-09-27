f89100e704312e63bc71cb838498ac38080d2cb6
Krushna Nagare <contact.nagare@gmail.com>
2023-09-27 23:37:04 +0530

    isuue

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const app = express()
 //     console.log("DB Connected successfully");
 // })
 
-app.use(express.json())
+// app.use(express.json())
 
 mongoose.connect('mongodb+srv://contactnagare:7cnRMbOBIZdDIbYg@cluster0.xihnvek.mongodb.net/?retryWrites=true&w=majority');
 
